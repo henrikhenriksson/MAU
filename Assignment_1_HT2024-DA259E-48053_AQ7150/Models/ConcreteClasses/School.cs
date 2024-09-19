5acae85190709595b2610ccd081cb8e891d61e59
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-09-19 11:01:18 +0200

    Created School subclass draft

---
+++ Assignment_1_HT2024-DA259E-48053_AQ7150/Models/ConcreteClasses/School.cs
@@ -0,0 +1,37 @@
+﻿using RealEstateApp.Models.AbstractClasses;
+using RealEstateApp.Models.SupportingClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateApp.Models.ConcreteClasses
+{
+    public class School : Institutional
+    {
+        public string EducationalStage;
+
+        public School(int id,
+                      Address address,
+                      string institutionType,
+                      string numberOfDepartments,
+                      int maxCapacity, string educationalStage) : base(id, address, institutionType, numberOfDepartments, maxCapacity)
+        {
+            EducationalStage = educationalStage;
+        }
+
+        public override void GetInfo()
+        {
+
+            Console.WriteLine($"School Info:" +
+                $"\nID: {ID}" +
+                $"\nAddress: {Address}" +
+                $"\nInstitution type: {InstitutionType}" +
+                $"\nEducational Stage: {EducationalStage}" +
+                $"Departments: {NumberOfDepartments}" +
+                $"Capacity: {MaxCapacity}");
+
+        }
+    }
+}
