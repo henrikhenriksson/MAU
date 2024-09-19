117494a0b464e95fc90dfe0ff7e454ede946c1bc
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-09-19 09:09:16 +0200

    Creating project structure

---
+++ Assignment_1_HT2024-DA259E-48053_AQ7150/Models/AbstractClasses/Estate.cs
@@ -0,0 +1,26 @@
+﻿using RealEstateApp.Models.Interfaces;
+using RealEstateApp.Models.SupportingClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateApp.Models.AbstractClasses
+{
+    public abstract class Estate : IEstate
+    {
+        public int ID { get; set; }
+        public Address Address { get; set; }
+
+
+        protected Estate(int id, Address address)
+        {
+            ID = id;
+            Address = address;
+        }
+
+        public abstract void GetInfo();
+
+    }
+}
---
+++ Assignment_1_HT2024-DA259E-48053_AQ7150/Models/Interfaces/IEstate.cs
@@ -0,0 +1,17 @@
+﻿using RealEstateApp.Models.SupportingClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateApp.Models.Interfaces
+{
+    public interface IEstate
+    {
+        int ID { get; set; }
+        Address Address { get; }
+        void GetInfo();
+
+    }
+}
