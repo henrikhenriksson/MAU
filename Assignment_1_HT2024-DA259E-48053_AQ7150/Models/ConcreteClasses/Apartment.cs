6bd5b12816cef1378997d2a1a272e1d827f554ab
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-09-19 11:00:30 +0200

    Created Apartment subclass draft

--- Assignment_1_HT2024-DA259E-48053_AQ7150/Models/ConcreteClasses/Apartment.cs
+++ Assignment_1_HT2024-DA259E-48053_AQ7150/Models/ConcreteClasses/Apartment.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RealEstateApp.Models.AbstractClasses;
+using RealEstateApp.Models.SupportingClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,41 @@ using System.Threading.Tasks;
 
 namespace RealEstateApp.Models.ConcreteClasses
 {
-    internal class Apartment
+    public class Apartment : Residential
     {
+        public Apartment(int id,
+                         Address address,
+                         int floorNumber,
+                         int numberOfRooms,
+                         double areal,
+                         bool hasGarden,
+                         bool hasGarage,
+                         bool hasBalcony) : base(id, address, numberOfRooms, areal, hasGarden, hasGarage)
+        {
+            FloorNumber = floorNumber;
+            HasBalcony = hasBalcony;
+        }
+
+        public int FloorNumber { get; set; }
+
+        public bool HasBalcony { get; set; }
+
+
+
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Apartment Info:" +
+                $"\nID: {ID}" +
+                $"\nAddress: {Address}" +
+                $"\nRooms: {NumberOfRooms}" +
+                $"\nArea: {Areal}" +
+                $"\nFloor Number: {FloorNumber}" +
+                $"\nProperties: " +
+                $"\nGarden : {HasGarden}" +
+                $"\nGarage: {HasGarage}" +
+                $"\nBalcony: {HasBalcony}"
+                );
+
+        }
     }
 }
