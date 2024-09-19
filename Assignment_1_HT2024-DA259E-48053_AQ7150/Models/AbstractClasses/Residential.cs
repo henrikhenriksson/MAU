0ae9c1ddbffec4cfc1e9ccfbbea319dd3781f23e
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-09-19 09:21:16 +0200

    Residential Abstract draft created

--- Assignment_1_HT2024-DA259E-48053_AQ7150/Models/AbstractClasses/Residential.cs
+++ Assignment_1_HT2024-DA259E-48053_AQ7150/Models/AbstractClasses/Residential.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RealEstateApp.Models.SupportingClasses;
 
 namespace RealEstateApp.Models.AbstractClasses
 {
-    internal class Residential
+    public abstract class Residential : Estate
     {
+        public int NumberOfRooms { get; set; }
+        public bool HasGarden { get; set; }
+        public bool HasGarage {  get; set; }
+
+        protected Residential(int id, Address address, int numberOfRooms, bool hasGarden, bool hasGarage)
+        : base(id, address) 
+        {
+            NumberOfRooms = numberOfRooms;
+            HasGarden = hasGarden;
+            HasGarage = hasGarage;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, Rooms: {NumberOfRooms}, Garden: {HasGarden}, Garage: {HasGarage}";
+        }
     }
 }
