4cf4c5fe42ec6c9e1a4a1e4fd966dab5958d88b7
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-09-26 13:11:47 +0200

    Added inherited concrete classes

---
+++ Assignment_1_HT2024-DA259E-48053_AQ7150/Models/ConcreteClasses/Rental.cs
@@ -0,0 +1,31 @@
+﻿using RealEstateApp.Models.SupportingClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateApp.Models.ConcreteClasses
+{
+    public class Rental : Apartment
+    {
+        public double Rent { get; set; }
+        public string LeasePeriod { get; set; }
+
+
+        public Rental(int id, Address address, int floorNumber, int numberOfRooms, double areal, bool hasGarden, bool hasGarage, bool hasBalcony, double rent, string leasePeriod) : base(id, address, floorNumber, numberOfRooms, areal, hasGarden, hasGarage, hasBalcony)
+        {
+            Rent = rent;
+            LeasePeriod = leasePeriod;
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo(); // Call base class GetInfo to print common Apartment info
+            Console.Write($"Monthly Rent: {Rent:C}" +
+                $" Lease Period: {LeasePeriod}"
+                );
+        }
+
+    }
+}
---
+++ Assignment_1_HT2024-DA259E-48053_AQ7150/Models/ConcreteClasses/Tenement.cs
@@ -0,0 +1,24 @@
+﻿using RealEstateApp.Models.SupportingClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateApp.Models.ConcreteClasses
+{
+    internal class Tenement : Apartment
+    {
+        public int MonthlyFee { get; set; }
+        public string HousingCooperative {  get; set; }
+
+      
+
+        public Tenement(int id, Address address, int floorNumber, int numberOfRooms, double areal, bool hasGarden, bool hasGarage, bool hasBalcony, int monthlyFee, string housingCooperative) : base(id, address, floorNumber, numberOfRooms, areal, hasGarden, hasGarage, hasBalcony)
+        {
+            MonthlyFee = monthlyFee;
+            HousingCooperative = housingCooperative;
+
+        }
+    }
+}
