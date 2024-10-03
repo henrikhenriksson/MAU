9479ae11ff5caf3be7195bf867075d695016474f
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-10-03 13:01:47 +0200

    Implemented PredictMyDay()

--- Assignment2HT24_HT2024-DA204E_AQ7150/ConsoleAppAss2/Classes/Assignment2B/StringFunctions.cs
+++ Assignment2HT24_HT2024-DA204E_AQ7150/ConsoleAppAss2/Classes/Assignment2B/StringFunctions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,20 +15,53 @@ namespace ConsoleAppAss2.Classes.Assignment2B
             
             
             Console.Title = "String Functions (Assignment 2B)";
-            Console.WriteLine("++++++++++++ String Functions +++++++++++");
-            string messagePrompt = "Write a text with any number of characters and press Enter.\nAlternatively, you can copy and paste a text into this window";
+            Console.WriteLine("++++++++++++ String Functions ++++++++++++");
+
+            Console.WriteLine("\n++++++++++++ String Length ++++++++++++");
+                        string messagePrompt = "Write a text with any number of characters and press Enter.\nAlternatively, you can copy and paste a text into this window";
             string inputMessage = Utility.ReadInput(messagePrompt);
-            Console.WriteLine($"\n{stringLength(inputMessage)}");
-           
+            Console.WriteLine($"\n{StringLength(inputMessage)}");
+            messagePrompt = ""; // reset to prevent undesired behaviour
+
+            Console.WriteLine("\n++++++++++++ The Fortune Teller ++++++++++++");
+            messagePrompt = "\nLet me predict your day!\nSelect a day between 1 and 7";
+            int selection = Utility.GetIntInput(messagePrompt,1,7);
+
+            Console.WriteLine(PredictMyDay(selection));
             
         }
 
-        private string stringLength(string inputMessage)
+
+
+        // this method returns a string with the input message in upper case and the number of chars in the string.
+        private string StringLength(string inputMessage)
         {
             return $"{inputMessage.ToUpper()} Number of Chars: {inputMessage.Length}";
         }
 
+        private string PredictMyDay(int selection)
+        {
 
+            switch (selection)
+            {
+                case 1:
+                    return "Keep calm on Mondays! You can fall apart!";
+                case 2:
+                    return "Tuesdays and Wednesdays break your heart!";
+                case 3:
+                    return "Tuesdays and Wednesdays break your heart!";
+                case 4:
+                    return "Thursday is your lucky day, don't wait for Friday!";
+                case 5:
+                    return "Friday, you are in love!";
+                case 6:
+                    return "Saturday, do nothing and do plenty of it!";
+                case 7:
+                    return "And Sunday always comes too soon!";
+                default:
+                    return "No day? A good day but it doesn't exist!";
+            }
+        }
 
     }
 }
