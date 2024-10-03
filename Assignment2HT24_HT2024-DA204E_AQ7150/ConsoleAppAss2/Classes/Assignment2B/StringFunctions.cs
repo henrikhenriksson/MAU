16dd5948cb7f0ed9ad8a218b7b34c3306417b69d
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-10-03 13:17:07 +0200

    Implemented RunAgain()

--- Assignment2HT24_HT2024-DA204E_AQ7150/ConsoleAppAss2/Classes/Assignment2B/StringFunctions.cs
+++ Assignment2HT24_HT2024-DA204E_AQ7150/ConsoleAppAss2/Classes/Assignment2B/StringFunctions.cs
@@ -12,23 +12,50 @@ namespace ConsoleAppAss2.Classes.Assignment2B
 
         public void Start()
         {
-            
-            
-            Console.Title = "String Functions (Assignment 2B)";
-            Console.WriteLine("++++++++++++ String Functions ++++++++++++");
 
-            Console.WriteLine("\n++++++++++++ String Length ++++++++++++");
-                        string messagePrompt = "Write a text with any number of characters and press Enter.\nAlternatively, you can copy and paste a text into this window";
-            string inputMessage = Utility.ReadInput(messagePrompt);
-            Console.WriteLine($"\n{StringLength(inputMessage)}");
-            messagePrompt = ""; // reset to prevent undesired behaviour
+            bool continueRunning = false;
 
-            Console.WriteLine("\n++++++++++++ The Fortune Teller ++++++++++++");
-            messagePrompt = "\nLet me predict your day!\nSelect a day between 1 and 7";
-            int selection = Utility.GetIntInput(messagePrompt,1,7);
+            do
+            {
+                Console.Title = "String Functions (Assignment 2B)";
+                Console.WriteLine("++++++++++++ String Functions ++++++++++++");
 
-            Console.WriteLine(PredictMyDay(selection));
-            
+                Console.WriteLine("\n++++++++++++ String Length ++++++++++++");
+                string messagePrompt = "Write a text with any number of characters and press Enter.\nAlternatively, you can copy and paste a text into this window";
+                string inputMessage = Utility.ReadInput(messagePrompt).Trim(); // Trim any whitespace chars that might be copied.
+                Console.WriteLine($"\n{StringLength(inputMessage)}");
+                messagePrompt = ""; // reset to prevent undesired behaviour
+
+                Console.WriteLine("\n++++++++++++ The Fortune Teller ++++++++++++");
+                messagePrompt = "\nLet me predict your day!\nSelect a day between 1 and 7";
+                int selection = Utility.GetIntInput(messagePrompt, 1, 7);
+
+                Console.WriteLine(PredictMyDay(selection));
+
+                continueRunning = RunAgain();
+            }
+            while (continueRunning);
+        }
+
+        private bool RunAgain()
+        {
+            while (true)
+            { // run forever until a valid input is read
+
+                string input = Utility.ReadInput("\nWould you like to continue running this program?");
+                if (input.ToLower() == "y")
+                {
+                    return true;
+                }
+                else if (input.ToLower() == "n")
+                { return false; }
+                else
+                {
+                    Console.WriteLine("Invalid input, enter 'y/Y' for yes, or 'n/N' for no");
+                }
+            }
+
+
         }
 
 
