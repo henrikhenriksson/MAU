f8fa161619af2d7f2a0686d1e8d7d56eb922e7ab
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-10-04 10:32:27 +0200

    Implementing scheduler methods

--- Assignment2HT24_HT2024-DA204E_AQ7150/ConsoleAppAss2/Classes/Assignment2D/Scheduler.cs
+++ Assignment2HT24_HT2024-DA204E_AQ7150/ConsoleAppAss2/Classes/Assignment2D/Scheduler.cs
@@ -8,5 +8,43 @@ namespace ConsoleAppAss2.Classes.Assignment2D
 {
     internal class Scheduler
     {
+        private const int weeksInYear = 52;
+        private List<int> WeeksWithWeekendShifts = new List<int>();
+        private List<int> WeeksWithNightShifts = new List<int>();
+
+
+        // Entry point
+        public void Run()
+        {
+            GenerateShiftWeekends();
+            GenerateShiftNights();
+
+
+        }
+
+        private void GenerateShiftNights()
+        {
+            // Night shifts increment of 4 starting first week
+            for (int week = 1; week <= weeksInYear; week+=4)
+            {
+                WeeksWithNightShifts.Add(week);
+            }
+
+        }
+
+        private void GenerateShiftWeekends()
+        {
+
+            // Weekendshifts based on an increment of 2 for every other weekend
+            for (int week = 2; week <= weeksInYear; week += 2)
+            {
+                WeeksWithWeekendShifts.Add(week);
+            }
+        }
+
+        private void displaySchedule(string shiftType)
+        {
+
+        }
     }
 }
