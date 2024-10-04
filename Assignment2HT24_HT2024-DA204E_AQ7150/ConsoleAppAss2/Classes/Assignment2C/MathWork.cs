181c69e6bbea3583e56db40744ddb97d1e63cc21
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-10-04 10:20:47 +0200

    Final Changes

--- Assignment2HT24_HT2024-DA204E_AQ7150/ConsoleAppAss2/Classes/Assignment2C/MathWork.cs
+++ Assignment2HT24_HT2024-DA204E_AQ7150/ConsoleAppAss2/Classes/Assignment2C/MathWork.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Written by: Henrik Henriksson(AQ7150)
 
 namespace ConsoleAppAss2.Classes.Assignment2C
 {
---
+++ Assignment2HT24_HT2024-DA204E_AQ7150/ConsoleAppAss2/Classes/Assignment2D/Scheduler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAss2.Classes.Assignment2D
+{
+    internal class Scheduler
+    {
+    }
+}
