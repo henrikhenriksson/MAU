204a114084f49360874376743233c1a9a28a8223
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-10-10 09:47:04 +0200

    Implemented Person class with public properties

---
+++ Assignment3_HT2024-DA204E_AQ7150/Classes/Person.cs
@@ -0,0 +1,85 @@
+﻿using Assignment3_HT2024_DA204E_AQ7150.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_HT2024_DA204E_AQ7150.Classes
+{
+
+    /// <summary>
+    /// This class utilizes private instance variables and public properties to ensure better controll of access to the properties of the class.
+    /// </summary>
+    public class Person
+    {
+        private double _height;
+        private double _weight;
+        private GenderEnum _gender;
+        private ActivityLevel _activityLevel;
+
+        public double Height
+        {
+            get { return _height; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Height must be greater than zero.");
+                }
+
+                _height = value;
+
+            }
+
+
+        }
+
+        public double Weight
+        {
+            get { return _weight; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Height must be greater than zero.");
+                }
+
+                _weight = value;
+
+            }
+
+
+        }
+
+        public GenderEnum Gender
+        {
+            get { return _gender; }
+            set { Gender = value; }
+        }
+
+        public ActivityLevel ActivityLevel
+        {
+            get { return _activityLevel; }
+            set { _activityLevel = value; }
+        }
+
+
+
+
+
+        public Person(double height,
+                      double width,
+                      GenderEnum gender,
+                      ActivityLevel activityLevel)
+        {
+            this._height = height;
+            this._weight = width;
+            this._gender = gender;
+            this._activityLevel = activityLevel;
+        }
+
+
+
+    }
+}
