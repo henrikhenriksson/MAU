74ce4fab181d1a3cdedbf1239bde6a2ac8be3e13
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-10-10 09:46:50 +0200

    Created Enum structure

---
+++ Assignment3_HT2024-DA204E_AQ7150/Enums/ActivityLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_HT2024_DA204E_AQ7150.Enums
+{
+    public enum ActivityLevel
+    {
+        Low,
+        Medium,
+        High
+
+    }
+}
