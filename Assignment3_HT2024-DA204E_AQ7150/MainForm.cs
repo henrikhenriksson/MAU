cdeb1e516d93d71082fc699b2af89d305124656e
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-10-10 09:07:05 +0200

    initial commit

---
+++ Assignment3_HT2024-DA204E_AQ7150/MainForm.cs
@@ -0,0 +1,10 @@
+namespace Assignment3_HT2024_DA204E_AQ7150
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
