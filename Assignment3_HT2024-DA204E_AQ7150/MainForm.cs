89e0d46fef93156472a28629dc328ae26480b675
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-10-15 12:19:13 +0200

    Labels and Textboxes in place.

--- Assignment3_HT2024-DA204E_AQ7150/MainForm.cs
+++ Assignment3_HT2024-DA204E_AQ7150/MainForm.cs
@@ -1,5 +1,8 @@
 
 
+// Written by : Henrik Henriksson(AQ7150)
+// Ignore Spelling: AQ
+
 using Assignment3_HT2024_DA204E_AQ7150.Classes;
 using Assignment3_HT2024_DA204E_AQ7150.Enums;
 
@@ -17,18 +20,30 @@ namespace Assignment3_HT2024_DA204E_AQ7150
         {
             Text = "Water Intake Calculator";
             StartPosition = FormStartPosition.CenterScreen;
-            FormBorderStyle = FormBorderStyle.None;
+            FormBorderStyle = FormBorderStyle.FixedDialog;
             MaximizeBox = false;
             BackColor = Color.DarkGray;
+            Size = new Size(420, 500);
 
 
             Color labelTextColor = Color.White;
             Font commonFont = new("Arial", 10, FontStyle.Regular);
 
+            // Panel for the calculator:
+            Panel pnlWaterIntakeCalculator = new Panel()
+            {
+                Location = new Point(10, 10),
+                Size = new Size(380, 400),
+                BorderStyle = BorderStyle.FixedSingle,
+                BackColor = Color.DarkSlateGray
+
+            };
+            Controls.Add(pnlWaterIntakeCalculator);
+
             GroupBox grpUnits = new GroupBox()
             {
                 Text = "Select Units",
-                Location = new Point(20, 20),
+                Location = new Point(10, 10),
                 Size = new Size(350, 70),
                 ForeColor = labelTextColor,
                 Font = commonFont,
@@ -58,9 +73,29 @@ namespace Assignment3_HT2024_DA204E_AQ7150
             // add them to the control
             grpUnits.Controls.Add(rdoMetric);
             grpUnits.Controls.Add(rdoImperial);
-            // add the
+            // add the grpbox to panel
             // 
-            Controls.Add(grpUnits);
+            pnlWaterIntakeCalculator.Controls.Add(grpUnits);
+
+            Label lblName = new Label()
+            {
+                Text = "Name: ",
+                Location = new Point(20, 90),
+                ForeColor = labelTextColor,
+                Font = commonFont
+            };
+
+            // TODO: Add and show ft&in if imperial is checked.
+
+            TextBox txtName = new TextBox()
+            {
+                Name = "txtName",
+                Location = new Point(120, 90),
+                BackColor = labelTextColor,
+                Font = commonFont,
+                Width = 200
+
+            };
 
             Label lblHeight = new Label()
             {
@@ -74,10 +109,11 @@ namespace Assignment3_HT2024_DA204E_AQ7150
                 Name = "TxtHeight",
                 Location = new Point(120, 100),
                 BackColor = Color.LightGray,
-                Font = commonFont
+                Font = commonFont,
+                Width = 200
             };
-            Controls.Add(lblHeight);
-            Controls.Add(txtHeight);
+            pnlWaterIntakeCalculator.Controls.Add(lblHeight);
+            pnlWaterIntakeCalculator.Controls.Add(txtHeight);
 
 
             Label lblWeight = new Label()
@@ -91,13 +127,21 @@ namespace Assignment3_HT2024_DA204E_AQ7150
             TextBox txtWeight = new TextBox()
             {
                 Name = "txtWeight",
-                Location = new Point(20, 140),
+                Location = new Point(120, 140),
                 BackColor = Color.LightGray,
+                Font = commonFont,
+                Width = 200
+            };
+            pnlWaterIntakeCalculator.Controls.Add(lblWeight);
+            pnlWaterIntakeCalculator.Controls.Add(txtWeight);
+
+            Label lblGender = new Label()
+            {
+                Text = "Gender:",
+                Location = new Point(20, 180),
+                ForeColor = Color.LightGray,
                 Font = commonFont
             };
-            Controls.Add(lblWeight);
-            Controls.Add(txtWeight);
-
 
             ComboBox cmbGender = new ComboBox()
             {
@@ -105,22 +149,35 @@ namespace Assignment3_HT2024_DA204E_AQ7150
                 Location = new Point(120, 180),
                 DropDownStyle = ComboBoxStyle.DropDownList,
                 BackColor = Color.LightGray,
-                Font = commonFont
+                Font = commonFont,
+                Width = 200
             };
             cmbGender.Items.AddRange(Enum.GetNames(typeof(GenderEnum)));
-            Controls.Add(cmbGender);
+            pnlWaterIntakeCalculator.Controls.Add(lblGender);
+            pnlWaterIntakeCalculator.Controls.Add(cmbGender);
+
+            Label lblActitivyLevel = new Label()
+            {
+                Text = "Activity Level:",
+                Location = new Point(20, 220),
+                ForeColor = Color.LightGray,
+                Font = commonFont
+            };
 
 
             ComboBox cmbActivityLevel = new ComboBox()
             {
                 Name = "cmbActivityLevel",
-                Location = new Point(120, 120),
+                Location = new Point(120, 220),
                 DropDownStyle = ComboBoxStyle.DropDownList,
                 BackColor = Color.LightGray,
-                Font = commonFont
+                Font = commonFont,
+                Width = 200
             };
+
             cmbActivityLevel.Items.AddRange(Enum.GetNames(typeof(ActivityLevel)));
-            Controls.Add(cmbActivityLevel);
+            pnlWaterIntakeCalculator.Controls.Add(lblActitivyLevel);
+            pnlWaterIntakeCalculator.Controls.Add(cmbActivityLevel);
 
             Button btnCalculate = new Button()
             {
@@ -128,19 +185,21 @@ namespace Assignment3_HT2024_DA204E_AQ7150
                 Location = new Point(20, 260),
                 BackColor = Color.Gray,
                 ForeColor = labelTextColor,
-                Font = commonFont
+                Font = commonFont,
+                Width= 300
             };
             btnCalculate.Click += btnCalculate_Click;
-            Controls.Add(btnCalculate);
+            pnlWaterIntakeCalculator.Controls.Add(btnCalculate);
 
             Label lblResult = new Label()
             {
                 Name = "lblResult",
                 Text = "Daily Water Intake",
-                Location= new Point(20,300),
+                Location = new Point(20, 300),
                 AutoSize = true,
-                ForeColor= labelTextColor,
+                ForeColor = labelTextColor,
                 Font = commonFont
+                
             };
             this.Controls.Add(lblResult);
 
@@ -148,6 +207,19 @@ namespace Assignment3_HT2024_DA204E_AQ7150
 
         private void btnCalculate_Click(object? sender, EventArgs e)
         {
+            // get metric or imperial
+
+            // get values for height/weight
+
+            // check conversion or not
+
+
+            // get gender
+
+            // get actiivity
+
+            // create person
+
 
 
         }
