38fae48d47c01b31a97d6edca4da2479ed6a5c40
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-10-15 13:59:36 +0200

    Changed name for consistency

---
+++ Assignment3_HT2024-DA204E_AQ7150/Enums/Gender.cs
@@ -0,0 +1,17 @@
+﻿// Ignore Spelling: Enums AQ
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_HT2024_DA204E_AQ7150.Enums
+{
+    public enum Gender
+    {
+        Male,
+        Female
+
+    }
+}
