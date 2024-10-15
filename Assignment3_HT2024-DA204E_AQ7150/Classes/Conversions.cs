1942b320a6736b9d7fc8050ad188a1769fc6cbe3
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-10-15 10:56:42 +0200

    Created conversion helper class

---
+++ Assignment3_HT2024-DA204E_AQ7150/Classes/Conversions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_HT2024_DA204E_AQ7150.Classes
+{
+    public static class Conversions
+    {
+        // imperial to metric
+        public static double InchesToCm(double inches) => inches * 2.54;
+        public static double PoundsToKg(double pounds) => pounds * 0.453592;
+        public static double OuncesToMl(double ounces) => ounces / 0.033814;
+        
+        public static double CmToInches(double cm) => cm / 2.54;
+        public static double KgToPounds(double kg) => kg / 0.453592;
+        public static double MltoOunces(double ml) => ml * 0.033814;
+
+
+
+
+
+    }
+}
