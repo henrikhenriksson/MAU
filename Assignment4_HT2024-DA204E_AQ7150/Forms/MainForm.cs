4eceea5b3e50db74b69a63efbaafdaa96839d7e5
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-10-23 14:19:33 +0200

    Implemented remove method

--- Assignment4_HT2024-DA204E_AQ7150/Forms/MainForm.cs
+++ Assignment4_HT2024-DA204E_AQ7150/Forms/MainForm.cs
@@ -124,24 +124,121 @@ namespace Assignment4_HT2024_DA204E_AQ7150
             btnUpdateGuest.Click += BtnUpdateGuest_Click;
             mainPanel.Controls.Add(btnUpdateGuest);
 
+            Label lblTotalCost = new Label
+            {
+                Text = "Total Cost:",
+                Location = new System.Drawing.Point(20, 250),
+                AutoSize = true
+            };
+            mainPanel.Controls.Add(lblTotalCost);
+
+            TextBox txtTotalCost = new TextBox
+            {
+                Name = "txtTotalCost",
+                Location = new System.Drawing.Point(150, 250),
+                Size = new System.Drawing.Size(100, 20),
+                ReadOnly = true
+            };
+            mainPanel.Controls.Add(txtTotalCost);
+
+            Label lblTotalRevenue = new Label
+            {
+                Text = "Total Revenue:",
+                Location = new System.Drawing.Point(20, 290),
+                AutoSize = true
+            };
+            mainPanel.Controls.Add(lblTotalRevenue);
+
+            TextBox txtTotalRevenue = new TextBox
+            {
+                Name = "txtTotalRevenue",
+                Location = new System.Drawing.Point(150, 290),
+                Size = new System.Drawing.Size(100, 20),
+                ReadOnly = true
+            };
+            mainPanel.Controls.Add(txtTotalRevenue);
+
+            Label lblSurplusOrDeficit = new Label
+            {
+                Text = "Surplus/Deficit:",
+                Location = new System.Drawing.Point(20, 330),
+                AutoSize = true
+            };
+            mainPanel.Controls.Add(lblSurplusOrDeficit);
+
+            TextBox txtSurplusOrDeficit = new TextBox
+            {
+                Name = "txtSurplusOrDeficit",
+                Location = new System.Drawing.Point(150, 330),
+                Size = new System.Drawing.Size(100, 20),
+                ReadOnly = true
+            };
+            mainPanel.Controls.Add(txtSurplusOrDeficit);
+
+            Button btnCalculateFinancials = new Button
+            {
+                Text = "Calculate Financials",
+                Location = new System.Drawing.Point(20, 370),
+                Size = new System.Drawing.Size(150, 30)
+            };
+            btnCalculateFinancials.Click += new EventHandler(this.btnCalculateFinancials_Click);
+            mainPanel.Controls.Add(btnCalculateFinancials);
+
+        }
+
+        private void btnCalculateFinancials_Click(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void BtnUpdateGuest_Click(object? sender, EventArgs e)
         {
-            using(AddGuestForm addGuestForm = new(eventManager)) // use with resources to handle disposable.
+            ListBox lstGuests = (ListBox)this.Controls.Find("lstGuests", true)[0];
+
+            if (lstGuests.SelectedIndex >= 0)
             {
-                addGuestForm.ShowDialog();
+                using AddOrUpdateGuestForm updateGuestForm = new AddOrUpdateGuestForm(eventManager, lstGuests.SelectedIndex);
+
+                updateGuestForm.ShowDialog();
             }
+            else
+            {
+                MessageBox.Show("Please selct a guest to update.", "Selection Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+
         }
 
         private void BtnRemoveGuest_Click(object? sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            ListBox lstGuests = (ListBox)Controls.Find("lstGuests", true)[0];
+
+            if (lstGuests.SelectedIndex >= 0)
+            {
+                DialogResult result = MessageBox.Show("Are you sure you want to remove this guest?", "Confirm Removal", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    if (eventManager.RemoveGuest(lstGuests.SelectedIndex))
+                    {
+                        MessageBox.Show("Guest removed Succesfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    } else
+                    {
+
+                        MessageBox.Show("Unable to remove the selected guest.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a guest to remove.", "Selection Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
 
         private void BtnOpenGuestForm_Click(object? sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            using AddOrUpdateGuestForm addGuestForm = new(eventManager); // use with resources to handle disposable.
+            addGuestForm.ShowDialog();
         }
 
         private void BtnCreateList_Click(object? sender, EventArgs e)
