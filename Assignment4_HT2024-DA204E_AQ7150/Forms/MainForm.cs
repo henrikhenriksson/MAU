6e710d70f372e6ec138fa0660de11148fc17a6ae
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-10-24 15:04:05 +0200

    Styling and bugfixing

--- Assignment4_HT2024-DA204E_AQ7150/Forms/MainForm.cs
+++ Assignment4_HT2024-DA204E_AQ7150/Forms/MainForm.cs
@@ -20,6 +20,14 @@ namespace Assignment4_HT2024_DA204E_AQ7150
         private TextBox txtCostPerGuest;
         private TextBox txtFeePerGuest;
 
+        private Font commonFont = new Font("Arial", 10, FontStyle.Regular);
+        private Color commonBackColor = Color.DarkGray;
+        private Color commonForeColor = Color.Black;
+        private Button btnRemoveGuest;
+        private Button btnUpdateGuest;
+        private Button btnOpenGuestForm;
+        private Button btnCalculateFinancials;
+
         public MainForm()
         {
             InitializeComponent();
@@ -28,20 +36,24 @@ namespace Assignment4_HT2024_DA204E_AQ7150
 
         private void InitializeGUI()
         {
+            ForeColor = commonForeColor;
             Text = "Event Organizer ";
             Size = new Size(800, 600);
 
             Panel mainPanel = new()
             {
                 Size = new Size(760, 540),
-                Location = new Point(20, 20),
-                BorderStyle = BorderStyle.FixedSingle
+                Location = new Point((this.ClientSize.Width - 760) / 2, (this.ClientSize.Height - 540) / 2), // Center the panel
+                BorderStyle = BorderStyle.FixedSingle,
+                BackColor = Color.DarkSlateGray
 
             };
             Controls.Add(mainPanel);
 
             Label lblMaxGuests = new()
             {
+                Font = commonFont,
+                ForeColor = commonForeColor,
                 Text = "Max Guests: ",
                 Location = new Point(20, 20),
                 AutoSize = true,
@@ -49,6 +61,9 @@ namespace Assignment4_HT2024_DA204E_AQ7150
             mainPanel.Controls.Add(lblMaxGuests);
             txtMaxGuest = new()
             {
+                Font = commonFont,
+                BackColor = commonBackColor,
+                ForeColor = commonForeColor,
                 Name = "txtMaxGuests",
                 Location = new Point(150, 20),
                 Size = new Size(100, 20)
@@ -57,7 +72,8 @@ namespace Assignment4_HT2024_DA204E_AQ7150
 
             Label lblCostPerGuest = new()
             {
-
+                Font = commonFont,
+                ForeColor = commonForeColor,
                 Text = "Cost per Guest: ",
                 Location = new Point(20, 60),
                 AutoSize = true
@@ -67,6 +83,9 @@ namespace Assignment4_HT2024_DA204E_AQ7150
 
             txtCostPerGuest = new()
             {
+                Font = commonFont,
+                BackColor = commonBackColor,
+                ForeColor = commonForeColor,
                 Name = "txtCostPerGuest",
                 Location = new Point(150, 60),
                 Size = new Size(100, 20)
@@ -75,6 +94,8 @@ namespace Assignment4_HT2024_DA204E_AQ7150
 
             Label lblFeePerGuest = new()
             {
+                Font = commonFont,
+                ForeColor = commonForeColor,
                 Text = "FeePerGuest",
                 Location = new Point(20, 100),
                 AutoSize = true
@@ -83,6 +104,9 @@ namespace Assignment4_HT2024_DA204E_AQ7150
 
             txtFeePerGuest = new()
             {
+                Font = commonFont,
+                BackColor = commonBackColor,
+                ForeColor = commonForeColor,
                 Name = "txtFeePerGuest",
                 Location = new Point(150, 100),
                 Size = new Size(100, 20)
@@ -91,6 +115,9 @@ namespace Assignment4_HT2024_DA204E_AQ7150
 
             Button btnCreateList = new()
             {
+                Font = commonFont,
+                ForeColor = commonForeColor,
+                BackColor = commonBackColor,
                 Text = "Create Guest List",
                 Location = new Point(20, 140),
                 Size = new Size(150, 30)
@@ -98,43 +125,61 @@ namespace Assignment4_HT2024_DA204E_AQ7150
             btnCreateList.Click += BtnCreateList_Click;
             mainPanel.Controls.Add(btnCreateList);
 
-            Button btnOpenGuestForm = new()
+            btnOpenGuestForm = new()
             {
+                Font = commonFont,
+                ForeColor = commonForeColor,
+                BackColor = commonBackColor,
                 Text = "Add Guest",
                 Location = new Point(20, 190),
-                Size = new Size(150, 30)
+                Size = new Size(150, 30),
+                Enabled = false
             };
             btnOpenGuestForm.Click += BtnOpenGuestForm_Click;
             mainPanel.Controls.Add(btnOpenGuestForm);
 
             ListBox lstGuests = new()
             {
+                Font = commonFont,
+                ForeColor = commonForeColor,
+                BackColor = commonBackColor,
                 Name = "lstGuests",
                 Location = new Point(400, 20),
                 Size = new Size(300, 320)
             };
             mainPanel.Controls.Add(lstGuests);
 
-            Button btnRemoveGuest = new()
+            btnRemoveGuest = new()
             {
+                Font = commonFont,
+                ForeColor = commonForeColor,
+                BackColor = commonBackColor,
                 Text = "Remove Guest",
                 Location = new Point(400, 360),
-                Size = new Size(120, 30)
+                Size = new Size(120, 30),
+                Enabled = false
             };
             btnRemoveGuest.Click += BtnRemoveGuest_Click;
             mainPanel.Controls.Add(btnRemoveGuest);
 
-            Button btnUpdateGuest = new()
+            btnUpdateGuest = new()
             {
+                Font = commonFont,
+                ForeColor = commonForeColor,
+                BackColor = commonBackColor,
                 Text = "Update Guest",
                 Location = new Point(580, 360),
-                Size = new Size(120, 30)
+                Size = new Size(120, 30),
+                Enabled = false
+
             };
             btnUpdateGuest.Click += BtnUpdateGuest_Click;
             mainPanel.Controls.Add(btnUpdateGuest);
 
             Label lblTotalCost = new Label
             {
+                Font = commonFont,
+                ForeColor = commonForeColor,
                 Text = "Total Cost:",
                 Location = new System.Drawing.Point(20, 250),
                 AutoSize = true
@@ -143,6 +188,9 @@ namespace Assignment4_HT2024_DA204E_AQ7150
 
             txtTotalCost = new TextBox
             {
+                Font = commonFont,
+                BackColor = commonBackColor,
+                ForeColor = commonForeColor,
                 Name = "txtTotalCost",
                 Location = new System.Drawing.Point(150, 250),
                 Size = new System.Drawing.Size(100, 20),
@@ -152,6 +200,8 @@ namespace Assignment4_HT2024_DA204E_AQ7150
 
             Label lblTotalRevenue = new Label
             {
+                Font = commonFont,
+                ForeColor = commonForeColor,
                 Text = "Total Revenue:",
                 Location = new System.Drawing.Point(20, 290),
                 AutoSize = true
@@ -160,6 +210,9 @@ namespace Assignment4_HT2024_DA204E_AQ7150
 
             txtTotalRevenue = new TextBox
             {
+                Font = commonFont,
+                BackColor = commonBackColor,
+                ForeColor = commonForeColor,
                 Name = "txtTotalRevenue",
                 Location = new System.Drawing.Point(150, 290),
                 Size = new System.Drawing.Size(100, 20),
@@ -169,6 +222,8 @@ namespace Assignment4_HT2024_DA204E_AQ7150
 
             Label lblSurplusOrDeficit = new Label
             {
+                Font = commonFont,
+                ForeColor = commonForeColor,
                 Text = "Surplus/Deficit:",
                 Location = new System.Drawing.Point(20, 330),
                 AutoSize = true
@@ -177,6 +232,9 @@ namespace Assignment4_HT2024_DA204E_AQ7150
 
             txtSurplusOrDeficit = new TextBox
             {
+                Font = commonFont,
+                BackColor = commonBackColor,
+                ForeColor = commonForeColor,
                 Name = "txtSurplusOrDeficit",
                 Location = new System.Drawing.Point(150, 330),
                 Size = new System.Drawing.Size(100, 20),
@@ -184,11 +242,16 @@ namespace Assignment4_HT2024_DA204E_AQ7150
             };
             mainPanel.Controls.Add(txtSurplusOrDeficit);
 
-            Button btnCalculateFinancials = new Button
+            btnCalculateFinancials = new Button
             {
+                Font = commonFont,
+                ForeColor = commonForeColor,
+                BackColor = commonBackColor,
                 Text = "Calculate Financials",
                 Location = new System.Drawing.Point(20, 370),
-                Size = new System.Drawing.Size(150, 30)
+                Size = new System.Drawing.Size(150, 30),
+                Enabled = false
+
             };
             btnCalculateFinancials.Click += new EventHandler(this.btnCalculateFinancials_Click);
             mainPanel.Controls.Add(btnCalculateFinancials);
@@ -224,48 +287,56 @@ namespace Assignment4_HT2024_DA204E_AQ7150
 
         private void BtnUpdateGuest_Click(object? sender, EventArgs e)
         {
-
-            if (lstGuests.SelectedIndex >= 0)
+            try
             {
-                using AddOrUpdateGuestForm updateGuestForm = new AddOrUpdateGuestForm(eventManager, lstGuests.SelectedIndex);
 
-                updateGuestForm.ShowDialog();
-                UpdateFinancials();
+                if (lstGuests != null && lstGuests.SelectedIndex >= 0)
+                {
+                    using AddOrUpdateGuestForm updateGuestForm = new AddOrUpdateGuestForm(eventManager, lstGuests.SelectedIndex);
+
+                    updateGuestForm.ShowDialog();
+                    UpdateFinancials();
+                }
+                else
+                {
+                    MessageBox.Show("Please selct a guest to update.", "Selection Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                MessageBox.Show("Please selct a guest to update.", "Selection Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                MessageBox.Show("You must have at least one guest before updating","No guest to update", MessageBoxButtons.OK, MessageBoxIcon.Stop);
             }
-
 
         }
 
         private void BtnRemoveGuest_Click(object? sender, EventArgs e)
         {
 
-            if (lstGuests.SelectedIndex >= 0)
-            {
-                DialogResult result = MessageBox.Show("Are you sure you want to remove this guest?", "Confirm Removal", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                if (result == DialogResult.Yes)
+
+                if (lstGuests != null && lstGuests.SelectedIndex >= 0)
                 {
-                    if (eventManager.RemoveGuest(lstGuests.SelectedIndex))
+                    DialogResult result = MessageBox.Show("Are you sure you want to remove this guest?", "Confirm Removal", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)
                     {
-                        MessageBox.Show("Guest removed Succesfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        UpdateFinancials();
+                        if (eventManager.RemoveGuest(lstGuests.SelectedIndex))
+                        {
+                            MessageBox.Show("Guest removed Succesfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            UpdateFinancials();
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Unable to remove the selected guest.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
 
                     }
-                    else
-                    {
-
-                        MessageBox.Show("Unable to remove the selected guest.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-
                 }
-            }
-            else
-            {
-                MessageBox.Show("Please select a guest to remove.", "Selection Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-            }
+                else
+                {
+                    MessageBox.Show("Please select a guest to remove.", "Selection Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            
+           
         }
 
         private void BtnOpenGuestForm_Click(object? sender, EventArgs e)
@@ -279,8 +350,7 @@ namespace Assignment4_HT2024_DA204E_AQ7150
         private void BtnCreateList_Click(object? sender, EventArgs e)
         {
 
-            try
-            {
+          
 
                 // get inputs
                 // Parse 
@@ -301,19 +371,24 @@ namespace Assignment4_HT2024_DA204E_AQ7150
                     return;
                 }
 
-                eventManager = new(maxGuest);
-                eventManager.CostPerPerson = costPerGuest;
-                eventManager.FeePerPerson = feePerGuest;
+                // generate class object
+                eventManager = new(maxGuest)
+                {
+                    CostPerPerson = costPerGuest,
+                    FeePerPerson = feePerGuest
+                };
 
                 MessageBox.Show("Guest list created successfully! You can now add, update, or remove guests.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                UpdateFinancials();
+                // update and enable buttons for manipulation.
 
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"An error has occurred {ex.Message}", "Error Creating List", MessageBoxButtons.OK, MessageBoxIcon.Stop);
 
-            }
+                if (eventManager.GetGuestList().Length > 0) { UpdateFinancials(); }
+
+                btnUpdateGuest.Enabled = true;
+                btnRemoveGuest.Enabled = true;
+                btnOpenGuestForm.Enabled = true;
+                btnCalculateFinancials.Enabled = true;
+
         }
     }
 }
