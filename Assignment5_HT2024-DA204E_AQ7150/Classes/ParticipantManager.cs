6dd47af0c7edde0947f508845f5c72f09b7cb420
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-11-29 13:16:20 +0100

    updated from guestclass

--- Assignment5_HT2024-DA204E_AQ7150/Classes/ParticipantManager.cs
+++ Assignment5_HT2024-DA204E_AQ7150/Classes/ParticipantManager.cs
@@ -1,4 +1,4 @@
-﻿using Assignment4_HT2024_DA204E_AQ7150.Classes;
+﻿using Assignment5_HT2024_DA204E_AQ7150.Classes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,13 +21,19 @@ namespace EventOrganizerApp.Classes.Managers
         {
 
             // make sure the participant isnt null
-            if (participant != null)
+            if (participant == null)
             {
-                participants.Add(participant);
-                return true;
+                return false;
             }
 
-            return false;
+            if (ParticipantExists(participant.FirstName, participant.LastName, participant.Address.Street))
+            {
+                return false;
+            }
+                
+
+            participants.Add(participant);
+            return true;
 
         }
 
@@ -55,10 +61,21 @@ namespace EventOrganizerApp.Classes.Managers
         public int GetNumberOfParticipants() { return participants.Count; }
 
 
+        public string[] GetParticipantsInfo()
+        {
+            return participants.Select(p => p.ToString()).ToArray();
+        }
+
         // check if valid index
         private bool CheckIndex(int index)
         {
             return index >= 0 && index < participants.Count;
+        }
+
+        public bool ParticipantExists(string firstName, string lastName, string street)
+        {
+            return participants.Any(p => p.FirstName.Equals(firstName, StringComparison.OrdinalIgnoreCase)
+            && p.LastName.Equals(lastName, StringComparison.OrdinalIgnoreCase) && p.Address.Street.Equals(street, StringComparison.OrdinalIgnoreCase)); // i'll be damned if 2 persons with the same name live on addresses with the same street name in different cities or something. Can of course be expanded upon if one was inclined.
         }
 
     }
