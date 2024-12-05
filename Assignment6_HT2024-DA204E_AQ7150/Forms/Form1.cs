c08f07d0af6774939d3c36b78f04cc632297d8b5
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-12-05 19:56:40 +0100

    initial commit

---
+++ Assignment6_HT2024-DA204E_AQ7150/Forms/Form1.cs
@@ -0,0 +1,15 @@
+namespace Assignment6_HT2024_DA204E_AQ7150
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
