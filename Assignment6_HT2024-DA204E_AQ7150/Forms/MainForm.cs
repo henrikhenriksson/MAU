e10a77df4d6a07744d3612cad8c0d468ac31fb2a
Henrik Henriksson <henrikhenriksson87@gmail.com>
2024-12-19 11:10:39 +0100

    Implemented mainform logic

--- Assignment6_HT2024-DA204E_AQ7150/Forms/MainForm.cs
+++ Assignment6_HT2024-DA204E_AQ7150/Forms/MainForm.cs
@@ -1,3 +1,6 @@
+// Written by: Henrik Henriksson(AQ7150)
+
+
 
 
 using Assignment6_HT2024_DA204E_AQ7150.Classes;
@@ -12,7 +15,6 @@ namespace Assignment6_HT2024_DA204E_AQ7150
 
         private MenuStrip menuStrip;
         private Panel mainPanel;
-        private TableLayoutPanel inputTable;
         private TextBox taskDescriptionTextBox;
         private DateTimePicker dueDatePicker;
         private ComboBox priorityComboBox;
@@ -21,6 +23,9 @@ namespace Assignment6_HT2024_DA204E_AQ7150
 
         private TaskManager taskManager;
 
+        Color commonBackColor;
+        Color commonForeColor;
+        Font commonFont;
         public MainForm()
         {
             taskManager = new TaskManager();
@@ -30,124 +35,170 @@ namespace Assignment6_HT2024_DA204E_AQ7150
 
         private void InitializeGUI()
         {
+            Text = "Task Manager";
+            StartPosition = FormStartPosition.CenterScreen;
+            Size = new Size(900, 700);
+            BackColor = Color.DarkGray;
+
+            commonFont = new Font("Arial", 10, FontStyle.Regular);
+            commonBackColor = Color.LightGray;
+            commonForeColor = Color.Black;
+
             InitializeMainPanel();
             initilizeMenuStrip();
             InitializeInputControls();
-            InitializeTaskListBox();
 
             MainMenuStrip = menuStrip;
             Controls.Add(menuStrip);
             Controls.Add(mainPanel);
-
-            Text = "Task Manager";
-            StartPosition = FormStartPosition.CenterScreen;
-            Size = new Size(800, 600);
-
         }
 
-        private void InitializeTaskListBox()
+
+        private void InitializeMainPanel()
         {
-
-            taskListBox = new ListBox()
+            mainPanel = new Panel
             {
-                Dock = DockStyle.Fill
+                Size = new Size(850, 600),
+                Location = new Point((this.ClientSize.Width - 850) / 2, (this.ClientSize.Height - 650) / 2 + 40),
+                BorderStyle = BorderStyle.FixedSingle,
+                BackColor = Color.SlateGray
             };
-
-
-            Label taskListLabel = new Label()
+        }
+        private void InitializeInputControls()
+        {
+            Label descriptionLabel = new Label
             {
-                Text = "Tasks: ",
+                Text = "Description",
+                Font = commonFont,
+                Location = new Point(20, 20),
                 AutoSize = true,
-                Dock = DockStyle.Top,
-                Padding = new Padding(10, 10, 0, 5)
             };
+            mainPanel.Controls.Add(descriptionLabel);
 
-            mainPanel.Controls.Add(taskListLabel);
+            taskDescriptionTextBox = new TextBox
+            {
+                Font = commonFont,
+                BackColor = commonBackColor,
+                ForeColor = commonForeColor,
+                Location = new Point(180, 20),
+                Size = new Size(200, 25)
+            };
+            mainPanel.Controls.Add(taskDescriptionTextBox);
 
+            Label dueDateLabel = new Label
+            {
+                Text = "Due Date: ",
+                Font = commonFont,
+                ForeColor = commonForeColor,
+                Location = new Point(20, 60),
+                AutoSize = true
+            };
+            mainPanel.Controls.Add(dueDateLabel);
+
+            dueDatePicker = new DateTimePicker
+            {
+                Value = DateTime.Now.AddHours(24),
+                Font = commonFont,
+                Format = DateTimePickerFormat.Custom,
+                CustomFormat = "dd/MM/yyyy hh:mm tt",
+                CalendarMonthBackground = commonBackColor,
+                CalendarForeColor = commonForeColor,
+                CalendarFont = commonFont,
+                Location = new Point(180, 60),
+                Size = new Size(200, 25)
+            };
+            mainPanel.Controls.Add(dueDatePicker);
+            Label priorityLabel = new Label
+            {
+                Text = "Priority",
+                Font = commonFont,
+                ForeColor = commonForeColor,
+                Location = new Point(20, 100),
+                AutoSize = true
+
+            };
+            mainPanel.Controls.Add(priorityLabel);
+            priorityComboBox = new ComboBox
+            {
+                Font = commonFont,
+                BackColor = commonBackColor,
+                ForeColor = commonForeColor,
+                Location = new Point(180, 100),
+                Size = new Size(200, 25),
+                DropDownStyle = ComboBoxStyle.DropDownList
+            };
+            priorityComboBox.Items.AddRange(Enum.GetNames(typeof(PriorityLevel)));
+            priorityComboBox.SelectedIndex = 2;
+            mainPanel.Controls.Add(priorityComboBox);
+
+            addTaskButton = new Button
+            {
+                Text = "Add Task:",
+                Font = commonFont,
+                BackColor = commonBackColor,
+                ForeColor = commonForeColor,
+                Location = new Point(20, 160),
+                Size = new Size(360, 30)
+            };
+            addTaskButton.Click += AddTaskButton_Click;
+            mainPanel.Controls.Add(addTaskButton);
+
+            Label taskListLabel = new Label
+            {
+                Text = "Tasks",
+                Font = commonFont,
+                ForeColor = commonForeColor,
+                Location = new Point(20, 230),
+                AutoSize = true
+            };
+            mainPanel.Controls[0].Controls.Add(taskListLabel);
+            taskListBox = new ListBox
+            {
+                Font = commonFont,
+                BackColor = commonBackColor,
+                ForeColor = commonForeColor,
+                Location = new Point(20, 270),
+                Size = new Size(780, 300)
+            };
             mainPanel.Controls.Add(taskListBox);
 
-
         }
+
+        private void initilizeMenuStrip()
+        {
+            menuStrip = new MenuStrip();
+            // File Menu section:
+
+            ToolStripMenuItem fileMenu = new ToolStripMenuItem("File");
+            ToolStripMenuItem newMenuItem = new ToolStripMenuItem("New", null, NewMenuItem_Click);
+            ToolStripMenuItem openMenuItem = new ToolStripMenuItem("Open", null, OpenMenuItem_Click);
+            ToolStripMenuItem saveMenuItem = new ToolStripMenuItem("Save", null, SaveMenuItem_Click);
+            ToolStripMenuItem exitMenuItem = new ToolStripMenuItem("Exit", null, ExitMenuItem_Click);
+
+            fileMenu.DropDownItems.Add(newMenuItem);
+            fileMenu.DropDownItems.Add(openMenuItem);
+            fileMenu.DropDownItems.Add(saveMenuItem);
+            fileMenu.DropDownItems.Add(new ToolStripSeparator());
+            fileMenu.DropDownItems.Add(exitMenuItem);
+
+            // Help Menu
+
+            ToolStripMenuItem helpMenu = new ToolStripMenuItem("Help");
+            ToolStripMenuItem aboutMenuItem = new ToolStripMenuItem("About", null, AboutMenuItem_Click);
+            helpMenu.DropDownItems.Add(aboutMenuItem);
+
+            menuStrip.Items.Add(fileMenu);
+            menuStrip.Items.Add(helpMenu);
+        }
+
 
         private void UpdateTaskListDisplay()
         {
             taskListBox.Items.Clear();
-            foreach (var task in taskManager.GetAllTasks())
+            foreach (var task in taskManager.GetAllTasks().OrderBy(t => t.DueDate))
             {
                 taskListBox.Items.Add(task.ToString());
             }
-        }
-
-        private void InitializeInputControls()
-        {
-            inputTable = new TableLayoutPanel
-            {
-                Dock = DockStyle.Top,
-                AutoSize = true,
-                ColumnCount = 2,
-                RowCount = 4,
-                Padding = new Padding(10),
-            };
-
-            Label descriptionLabel = new Label
-            {
-                Text = "Description:",
-                AutoSize = true,
-                TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-            };
-
-            Label dueDateLabel = new Label
-            {
-                Text = "Due Date:",
-                AutoSize = true,
-                TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-            };
-
-            Label priorityLabel = new Label
-            {
-                Text = "Priority:",
-                AutoSize = true,
-                TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-            };
-
-            taskDescriptionTextBox = new TextBox
-            {
-                PlaceholderText = "Enter Task Description",
-                Width = 200
-            };
-
-            dueDatePicker = new DateTimePicker
-            {
-                Format = DateTimePickerFormat.Custom,
-                CustomFormat = "dd/MM/yyyy hh:mm tt",
-                Width = 150
-            };
-
-            priorityComboBox = new ComboBox
-            {
-                DropDownStyle = ComboBoxStyle.DropDownList,
-                Width = 120
-
-            };
-            priorityComboBox.Items.AddRange(Enum.GetNames(typeof(PriorityLevel)));
-            priorityComboBox.SelectedIndex = 0;
-
-            addTaskButton = new Button
-            {
-                Text = "Add Task",
-                Width = 100
-            };
-            addTaskButton.Click += AddTaskButton_Click;
-
-            inputTable.Controls.Add(descriptionLabel, 0, 0);
-            inputTable.Controls.Add(taskDescriptionTextBox, 1, 0);
-            inputTable.Controls.Add(dueDateLabel, 0, 1);
-            inputTable.Controls.Add(dueDatePicker, 1, 1);
-            inputTable.Controls.Add(priorityLabel, 0, 2);
-            inputTable.Controls.Add(priorityComboBox, 1, 2);
-            inputTable.Controls.Add(addTaskButton, 1, 3);
-
-            mainPanel.Controls.Add(inputTable);
         }
 
         private void AddTaskButton_Click(object? sender, EventArgs e)
@@ -158,7 +209,7 @@ namespace Assignment6_HT2024_DA204E_AQ7150
                 string description = taskDescriptionTextBox.Text.Trim();
                 if (string.IsNullOrEmpty(description))
                 {
-                    MessageBox.Show("Description cannot be emtpy", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("Description cannot be empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     return;
                 }
 
@@ -184,8 +235,8 @@ namespace Assignment6_HT2024_DA204E_AQ7150
                 MessageBox.Show("Task added successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 // reset values for new entry.
                 taskDescriptionTextBox.Clear();
-                dueDatePicker.Value = DateTime.Now;
-                priorityComboBox.SelectedIndex = 0;
+                dueDatePicker.Value = DateTime.Now.AddHours(24);
+                priorityComboBox.SelectedIndex = 2;
             }
             catch (Exception ex)
             {
@@ -194,54 +245,70 @@ namespace Assignment6_HT2024_DA204E_AQ7150
 
         }
 
-        private void InitializeMainPanel()
-        {
-            mainPanel = new Panel { Dock = DockStyle.Fill };
-        }
-
-        private void initilizeMenuStrip()
-        {
-            menuStrip = new MenuStrip();
-            // File Menu section:
-
-            ToolStripMenuItem fileMenu = new ToolStripMenuItem("File");
-            ToolStripMenuItem newMenuItem = new ToolStripMenuItem("New", null, NewMenuItem_Click);
-            ToolStripMenuItem openMenuItem = new ToolStripMenuItem("Open", null, OpenMenuItem_Click);
-            ToolStripMenuItem saveMenuItem = new ToolStripMenuItem("Save", null, SaveMenuItem_Click);
-            ToolStripMenuItem exitMenuItem = new ToolStripMenuItem("File", null, ExitMenuItem_Click);
-
-            fileMenu.DropDownItems.Add(newMenuItem);
-            fileMenu.DropDownItems.Add(openMenuItem);
-            fileMenu.DropDownItems.Add(saveMenuItem);
-            fileMenu.DropDownItems.Add(new ToolStripSeparator());
-            fileMenu.DropDownItems.Add(exitMenuItem);
-
-            // Help Menu
-
-            ToolStripMenuItem helpMenu = new ToolStripMenuItem("Help");
-            ToolStripMenuItem aboutMenuItem = new ToolStripMenuItem("About", null, AboutMenuItem_Click);
-            helpMenu.DropDownItems.Add(aboutMenuItem);
-
-            menuStrip.Items.Add(fileMenu);
-            menuStrip.Items.Add(helpMenu);
-        }
-
         private void NewMenuItem_Click(object? sender, EventArgs e)
         {
-            MessageBox.Show("New menu clicked.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            var result = MessageBox.Show("Are you sure you want to clear the current list and start a new one?",
+                                         "Confirm New Task List",
+                                         MessageBoxButtons.YesNo,
+                                         MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                taskManager = new TaskManager();
+                UpdateTaskListDisplay();
+            }
         }
 
         private void OpenMenuItem_Click(object? sender, EventArgs e)
         {
-            MessageBox.Show("Open menu clicked.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            using OpenFileDialog openFileDialog = new OpenFileDialog(); ;
+
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog.Title = "Open Task File";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    FileHandler.LoadTasks(openFileDialog.FileName, taskManager);
+                    UpdateTaskListDisplay();
+                    MessageBox.Show("Tasks Loaded Succesfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An Error occured while loading the file: {ex.Message}", ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
 
         }
 
 
         private void SaveMenuItem_Click(object? sender, EventArgs e)
         {
-            MessageBox.Show("New menu clicked.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
+            if (!taskManager.GetAllTasks().Any())
+            {
+                MessageBox.Show("There are no tasks to save. Please add tasks before saving.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            saveFileDialog.Title = "Save Tasks to File";
+            saveFileDialog.FileName = $"Reminders_{DateTime.Now:yyyyMMdd_HHmm}";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    FileHandler.SaveTasks(saveFileDialog.FileName, taskManager);
+                    MessageBox.Show("Tasks saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred while saving the file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
 
         }
 
